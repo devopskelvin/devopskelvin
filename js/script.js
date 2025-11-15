f19932d583d606316a58f87acec423a18c61489a
Kelvin Alef <kelvin.alef@boavistapartners.com.br>
2025-11-15 19:51:09 -0300

    correcao no botão

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const titleElement = document.querySelector('.profile-title');
     if (titleElement) {
-        // Leia do atributo data-title, ou use textContent como fallback
         let originalText = titleElement.dataset.title || titleElement.textContent;
         
-        // FORÇA a substituição de espaços normais por espaços não-separáveis
         originalText = originalText.replace(/ /g, '\u00A0');
 
-        titleElement.textContent = ''; // Use textContent para escrever
+        titleElement.textContent = '';
         let i = 0;
         
         function typeWriter() {
             if (i < originalText.length) {
-                titleElement.textContent += originalText.charAt(i); // Use textContent para escrever
+                titleElement.textContent += originalText.charAt(i);
                 i++;
                 setTimeout(typeWriter, 150);
             }
